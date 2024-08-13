60d1768b2f27d6faf8aceb6e8094f0cfd24e9e39
fb.lb <fb.lubre@free.fr>
2024-08-13 21:35:10 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main>
+      </main>
+      <Footer/>
     </div>
   );
 }
